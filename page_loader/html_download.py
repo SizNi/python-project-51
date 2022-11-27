58a65951266d33693ec182530c00ddb2558c56fc
Nik Sizov <wwwwwwwww@list.ru>
2022-11-27 04:22:40 +0300

    remake test for mock and some fixes

--- page_loader/html_download.py
+++ page_loader/html_download.py
@@ -1,19 +1,17 @@
 import os
-import urllib.request
+import requests
 
 
 # отталкиваемся везде от текущей директории
 current_dir = os.getcwd()
 # используем для скачивания хтмл
-opener = urllib.request.FancyURLopener({})
 
 
 def download(url, save_dir=current_dir):
-    result = opener.open(url)
-    result = result.read()
+    result = requests.get(url)
+    result = result.text
     file_name = formatter(url)
     save_url = save_dir + '/' + file_name
-    print(save_url)
     f = open(f'{save_url}', 'w')
     f.write(f'{result}')
     f.close()
@@ -21,7 +19,7 @@ def download(url, save_dir=current_dir):
 
 def formatter(string):
     # обрезаем https/http
-    replace_list = ['.', '/', '%', '_']
+    replace_list = ['.', '/', '%', '_', ':']
     if string[:5] == 'https':
         string = string[8:]
     elif string[:5] == 'http:':
@@ -29,9 +27,9 @@ def formatter(string):
     # заменяет лишние элементы в url и добавляет .html
     for elem in replace_list:
         string = string.replace(elem, '-')
+    print(string + '.html')
     return (string + '.html')
 
 
-# url = '/home/ovechka/python-project-51/tests/fixtures/download.html'
 if __name__ == "__main__":
     download()
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,27 +1,13 @@
-import tempfile
-import os
 from page_loader.html_download import download
+import tempfile
 
 
-def test_html_download():
-    current_dir = os.getcwd()
-    url = f'{current_dir}/tests/fixtures/download.html'
+def test_download_html(requests_mock):
+    url = 'http://test.com'
+    requests_mock.get(url, text='data')
     with tempfile.TemporaryDirectory() as tmp:
-        tmp_dir_name = tmp
-        # запуск тестируемой функции с сохранением во временную директорию
-        download(url, tmp_dir_name)
-        file_way = (
-            f'{tmp_dir_name}/-home-ovechka-python'
-            '-project-51-tests-fixtures-download-html.html'
-        )
-        # открываем получившийся файл как актуальный
+        download(url, tmp)
+        file_way = (f'{tmp}/test-com.html')
         with open(file_way) as f:
             actual = f.read()
-            print(actual)
-            # открываем файл как ожидаемый
-            with open(
-                f'{current_dir}/tests/fixtures/test_download_explain.txt'
-            ) as f1:
-                expected = f1.read()
-                # сравниваем
-                assert actual == expected
+            assert actual == 'data'
